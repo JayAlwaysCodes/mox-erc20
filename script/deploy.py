06eea84c104cd3c15cfcfe9b6d347293539f73b4
jayalwayscodes <johnsonjardocs@gmail.com>
2025-04-03 19:19:41 +0100

    wrote total supply test

--- script/deploy.py
+++ script/deploy.py
@@ -1,13 +1,15 @@
 from contracts import myToken
 from eth_utils import to_wei
+from moccasin.boa_tools import VyperContract
 
 INITIAL_SUPPLY = to_wei(1000, "ether")
 
-def deploy():
+def deploy() -> VyperContract:
     snek_contract = myToken.deploy(INITIAL_SUPPLY)
     print(f"Token deployed at address: {snek_contract.address}")
     print(f"Token deployed with initial supply: {INITIAL_SUPPLY} wei")
+    return snek_contract
 
 
-def moccasin_main():
+def moccasin_main() -> VyperContract:
     return deploy()
---
+++ tests/test_token.py
@@ -0,0 +1,7 @@
+from script.deploy import deploy, INITIAL_SUPPLY
+
+def test_token_supply():
+    snek_token = deploy()
+
+    assert snek_token.totalSupply() == INITIAL_SUPPLY
+    
